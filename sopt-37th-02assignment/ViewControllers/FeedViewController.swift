b5e360caa12d0a309fb906185ebb30817d4fda0f
hyunseo <ols3040015@naver.com>
2025-11-14 01:02:47 +0900

    [feat] 스크롤시 헤더 고정

--- sopt-37th-02assignment/ViewControllers/FeedViewController.swift
+++ sopt-37th-02assignment/ViewControllers/FeedViewController.swift
@@ -9,13 +9,13 @@ import UIKit
 import SnapKit
 import Then
 
-public final class FeedViewController: UIViewController {
+public final class FeedViewController: UIViewController, UIScrollViewDelegate {
     
     // 1. 네비게이션 바 아이템
     private lazy var titleButton = UIButton(type: .system).then {
         $0.setTitle("우리집", for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
-        $0.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        $0.setImage(UIImage(named: "polygon1"), for: .normal)
         $0.tintColor = .black
         $0.semanticContentAttribute = .forceRightToLeft
     }
@@ -41,6 +41,7 @@ public final class FeedViewController: UIViewController {
         setupNav()
         setUI()
         setLayout()
+        scrollView.delegate = self
     }
     
     // 뷰의 크기가 정해진 후 그라데이션 크기 설정
@@ -65,22 +66,22 @@ public final class FeedViewController: UIViewController {
     
     private func setLayout() {
         
-        // --- 1. 고정 뷰 (검색창) ---
+        // 고정 뷰 (검색창)
         headerView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(48)
         }
         
-        // --- 2. 스크롤 뷰 ---
+        // 스크롤 뷰
         scrollView.snp.makeConstraints {
             $0.top.equalTo(headerView.snp.bottom).offset(16)
             $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
         
-        // --- 3. 스크롤 뷰 내부 콘텐츠 ---
+        // 스크롤 뷰 내부 콘텐츠
         
-        // 3-1. B마트 배너
+        // B마트 배너
         bMartBannerView.snp.makeConstraints {
             $0.top.equalTo(scrollView.contentLayoutGuide.snp.top) // 스크롤 뷰의 시작
             $0.leading.trailing.equalTo(scrollView.contentLayoutGuide).inset(16)
@@ -101,14 +102,14 @@ public final class FeedViewController: UIViewController {
             $0.width.equalTo(scrollView.frameLayoutGuide)
         }
         
-        // 3-3. 스토어 카테고리
+        // 스토어 카테고리
         storeCategoryView.snp.makeConstraints {
             $0.top.equalTo(foodCategoryView.snp.bottom).offset(16)
             $0.leading.trailing.equalTo(scrollView.contentLayoutGuide)
             $0.width.equalTo(scrollView.frameLayoutGuide)
         }
         
-        // 3-4. 광고 배너
+        // 광고 배너
         bannerView.snp.makeConstraints {
             $0.top.equalTo(storeCategoryView.snp.bottom).offset(16)
             $0.leading.trailing.equalTo(scrollView.contentLayoutGuide).inset(16)
@@ -120,6 +121,22 @@ public final class FeedViewController: UIViewController {
             $0.leading.trailing.equalTo(scrollView.contentLayoutGuide)
             $0.width.equalTo(scrollView.frameLayoutGuide)
             $0.bottom.equalTo(scrollView.contentLayoutGuide.snp.bottom).inset(16)
+        }
+    }
+    
+    
+}
+// MARK: - UIScrollViewDelegate
+
+extension FeedViewController {
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yOffset = scrollView.contentOffset.y
+        
+        if yOffset > 0 {
+            navigationController?.setNavigationBarHidden(true, animated: true)
+        }
+        else {
+            navigationController?.setNavigationBarHidden(false, animated: true)
         }
     }
 }
